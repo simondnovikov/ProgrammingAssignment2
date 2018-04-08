9f02bc055040276c0d710bacb3715d0e2bde42b0
Simon Novikov <moriclanuser2000@gmail.com>
2018-04-08 12:31:35 +0300

    problems with newlines

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
+﻿## Put comments here that give an overall description of what your
+
 ## functions do
+
+
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv_x<- NULL
+  set <- function(y) {
+    x <<- y
+    inv_x<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse_x) inv_x <<- inverse_x
+  get_inverse <- function() inv_x
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
+
+
+
 ## Write a short comment describing this function
 
+
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  inv_x <- x$get_inverse()
+  if(!is.null(inv_x)) {
+    message("getting cached data")
+    return(inv_x)
+  }
+  data <- x$get()
+  inv_x <- solve(data, ...)
+  x$set_inverse(inv_x)
+  inv_x
+  
 }
